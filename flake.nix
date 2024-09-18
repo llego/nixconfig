cd480f04475ebdfd2cc016da02c70b31b1efa0eb
llego <github.login@cri.su>
2024-09-18 13:59:21 +0300

    new home-manager profile for headless jail

--- flake.nix
+++ flake.nix
@@ -22,13 +22,20 @@
       pkgs = import nixpkgs { inherit system; };
     in {
    
-   		# home-manager configuration
+   		# default home-manager configuration for laptop
       homeConfigurations.default = inputs.home-manager.lib.homeManagerConfiguration {
       	inherit pkgs;
         modules = [ ./hosts/default/home.nix ];
         extraSpecialArgs = { inherit inputs; };
       };
         
+      # home-manager configuration for jail on truenas
+      homeConfigurations."jail" = inputs.home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [ ./hosts/jail/home.nix ];
+        extraSpecialArgs = { inherit inputs; };
+      };
+
     	# NixOS configuration
     	nixosConfigurations.default = nixpkgs.lib.nixosSystem {
       	specialArgs = { inherit inputs;};
---
+++ hosts/jail/home.nix
@@ -0,0 +1,24 @@
+{ config, pkgs, inputs, ... }:
+
+{
+  # Let Home Manager install and manage itself
+  programs.home-manager.enable = true;
+
+  imports =
+    [ 
+      ../../hm-modules/zsh.nix
+      ../../hm-modules/cli-utils.nix
+      ../../hm-modules/lf.nix
+    ];
+    
+  home.username = "llego";
+	home.homeDirectory = "/home/llego";
+  home.sessionVariables = {
+  	EDITOR = "nano";
+  };
+  
+  fonts.fontconfig.enable = true;
+  
+  home.stateVersion = "24.05";
+    
+}
