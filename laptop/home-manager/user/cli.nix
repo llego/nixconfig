9afe47d2b02135a9d6475351d46221802b408aee
llego <github.login@cri.su>
2024-10-26 22:40:15 +0300

    lf config

--- laptop/home-manager/user/cli.nix
+++ laptop/home-manager/user/cli.nix
@@ -92,22 +92,12 @@
     };
 
     keybindings = {
-      "\\\"" = "";
       o = "";
       c = "mkdir";
       "." = "set hidden!";
-      "`" = "mark-load";
-      "\\'" = "mark-load";
       "<enter>" = "open";
 
-      do = "dragon-out";
-
-      "g~" = "cd";
-      gh = "cd";
-      "g/" = "/";
-
       ee = "editor-open";
-      V = ''$${pkgs.bat}/bin/bat --paging=always --theme=gruvbox "$f"'';
     };
 
     settings = {
