038d23642b946546be30d13cd276908181d6047d
llego <github.login@cri.su>
2024-11-10 22:07:46 +0200

    configure new ultra wide monitor

--- laptop/home-manager/user/kanshi.nix
+++ laptop/home-manager/user/kanshi.nix
@@ -10,8 +10,8 @@
         profile.outputs = [
           {
             criteria = "eDP-1";
-            scale = 2.0;
             status = "enable";
+            scale = 2.0;
           }
         ];
       }
@@ -21,11 +21,16 @@
           {
             criteria = "DP-1";
             status = "enable";
+            #mode = "5120x2160@30Hz"; # not working
+            #mode = "4320x1800"; # not quite working. laptop monitor stays on. has black bars on the sides
+            #mode = "2560x1080"; # working, no black bars, but everything is too big
+            mode = "3840x2160"; # workin, but has black bars on the sides
+            scale = 1.6;
           }
           {
             criteria = "eDP-1";
-            scale = 1.0;
             status = "disable";
+            scale = 2.0;
           }
         ];
       }
@@ -35,10 +40,11 @@
           {
             criteria = "DP-2";
             status = "enable";
+            #mode = "5120x2160@30Hz";
+            scale = 1.6;
           }
           {
             criteria = "eDP-1";
-            scale = 1.0;
             status = "disable";
           }
         ];
