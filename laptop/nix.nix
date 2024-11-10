fc4d6b296cd3304e78ea95c73f5dbb43c7331b94
llego <github.login@cri.su>
2024-11-10 22:31:54 +0200

    garbage collection

--- laptop/nix.nix
+++ laptop/nix.nix
@@ -7,6 +7,17 @@
     ];
   };
 
+  # Perform garbage collection weekly to maintain low disk usage
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 14d";
+  };
+
   # Required by nixd (LSP) when using flakes
   nix.nixPath = ["nixpkgs=${inputs.nixpkgs}"];
+
+  # Limit the number of generations to keep
+  boot.loader.systemd-boot.configurationLimit = 10;
+  # boot.loader.grub.configurationLimit = 10;
 }
