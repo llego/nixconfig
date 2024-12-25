6074b6fb7cd2d012af1dddda408241ecaef49546
llego <github.login@cri.su>
2024-12-25 13:42:44 +0200

    resolution settings on gaming pc

--- hosts/gaming/home-manager/default.nix
+++ hosts/gaming/home-manager/default.nix
@@ -1,6 +1,16 @@
 {username, ...}: {
   home-manager.users.${username} = {
     home.stateVersion = "24.11";
-    imports = [./kanshi.nix];
+
+    programs.niri.settings.outputs."HDMI-A-1" = {
+      enable = true;
+      scale = 1.6;
+      mode = {
+        width = 3840;
+        height = 2160;
+        refresh = 59.997;
+      };
+      variable-refresh-rate = true;
+    };
   };
 }
