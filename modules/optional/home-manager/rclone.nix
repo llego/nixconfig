be118943f1c9190adafad56a02e38eb2ed93476d
llego <github.login@cri.su>
2025-04-07 22:04:43 +0300

    rclone comment

--- modules/optional/home-manager/rclone.nix
+++ modules/optional/home-manager/rclone.nix
@@ -11,8 +11,15 @@
   xdg.configFile."rclone/rclone.conf".source = config.lib.file.mkOutOfStoreSymlink "/home/llego/rclone-secret.txt";
 
   /*
-    xdg.configFile."rclone/rclone.conf".text = ''
-  zzz
-    '';
+  xdg.configFile."rclone/rclone.conf".text = ''
+    [storj]
+    type = s3
+    provider = Storj
+    access_key_id = secret123456
+    secret_access_key = secret123456
+    endpoint = gateway.storjshare.io
+    chunk_size = 64Mi
+    disable_checksum: true%
+  '';
   */
 }
