882890814a7c15f7b300630a83a08254151c52b5
llego <github.login@cri.su>
2025-12-12 22:20:26 +0200

    fix home path

--- modules/optional/home-manager/downloaders/album-downloader.sh
+++ modules/optional/home-manager/downloaders/album-downloader.sh
@@ -1,14 +1,14 @@
 #!bash
 
 # Set variables
-BANDCAMP_HOME="${XDG_DATA_HOME:-${HOME}/nixconfig/modules/optional/home-manager/downloaders}"
+BANDCAMP_HOME="${HOME}/nixconfig/modules/optional/home-manager/downloaders"
 #BANDCAMP_HOME="${XDG_DATA_HOME:-${HOME}/bandcamp-collection}";
 
 CACHE="$BANDCAMP_HOME/bandcamp-collection-downloader.cache"
 COOKIE="$BANDCAMP_HOME/bandcamp.com_cookies.txt"
 
-BANDCAMP_MUSIC_PATH="${XDG_DATA_HOME:-${HOME}/Music/bandcamp}"
-TIDAL_MUSIC_PATH="${XDG_DATA_HOME:-${HOME}/Music/tidal}"
+BANDCAMP_MUSIC_PATH="${HOME}/Music/bandcamp"
+TIDAL_MUSIC_PATH="${HOME}/Music/tidal"
 
 REMOTE_HOST="llego@truenas.home"
 REMOTE_HOST_PATH="$REMOTE_HOST:/mnt/illby/transient/beets-import"
