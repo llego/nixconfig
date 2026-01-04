89c66b099ab81885c85701fbdbe79d4fbabd94df
llego <github.login@cri.su>
2026-01-04 22:00:23 +0200

    replace noctalia shell with dank material shell

--- flake.nix
+++ flake.nix
@@ -5,25 +5,20 @@
     extra-substituters = [
       "https://nix-community.cachix.org?priority=2"
       #"https://cuda-maintainers.cachix.org?priority=1"
-      # "https://niri.cachix.org?priority=1"
-      # "https://llego.cachix.org?priority=1"
     ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       #"cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
-      # "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
-      # "llego.cachix.org-1:WzO82OCKQr+mNapPewBwEeN5Ui5vPjduTIYfrD0YFwQ="
     ];
   };
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
-    home-manager.url = "github:nix-community/home-manager/release-25.11";
+    # nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs";
+    # home-manager.url = "github:nix-community/home-manager/release-25.11";
+    home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    #stylix.url = "github:danth/stylix/release-25.11";
-    #niri.url = "github:sodiboo/niri-flake";
-    # nix-flatpak.url = "github:gmodena/nix-flatpak";
-    bandsnatch.url = "github:ovyerus/bandsnatch";
+    # bandsnatch.url = "github:ovyerus/bandsnatch";
     raspberry-pi-nix.url = "github:nix-community/raspberry-pi-nix";
     noctalia.url = "github:noctalia-dev/noctalia-shell";
     noctalia.inputs.nixpkgs.follows = "nixpkgs";
--- modules/optional/niri-config.nix
+++ modules/optional/niri-config.nix
@@ -6,14 +6,31 @@
   ...
 }: {
   imports = [
-    inputs.noctalia.nixosModules.default
+    # inputs.noctalia.nixosModules.default
   ];
 
   # Niri window manager
   programs.niri.enable = true;
 
+  # DankMaterialShell
+  programs.dms-shell = {
+    enable = true;
+
+    systemd = {
+      enable = true; # Systemd service for auto-start
+      restartIfChanged = true; # Auto-restart dms.service when dms-shell changes
+    };
+
+    # Core features
+    enableSystemMonitoring = true; # System monitoring widgets (dgop)
+    enableClipboard = true; # Clipboard history manager
+    enableVPN = true; # VPN management widget
+    enableDynamicTheming = true; # Wallpaper-based theming (matugen)
+    enableAudioWavelength = true; # Audio visualizer (cava)
+    enableCalendarEvents = true; # Calendar integration (khal)
+  };
   # Noctalia shell
-  services.noctalia-shell.enable = true;
+  # services.noctalia-shell.enable = true;
 
   environment.systemPackages = with pkgs; [
     pavucontrol
