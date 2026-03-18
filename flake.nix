23a4587bbd7a004abe8bbe6d28b68c0dd5e157d2
llego <github.login@cri.su>
2026-03-18 22:12:25 +0200

    flake: remove redundant nixConfig substituters (already in core.nix)

--- flake.nix
+++ flake.nix
@@ -1,17 +1,6 @@
 {
   description = "llego's nix config";
 
-  nixConfig = {
-    extra-substituters = [
-      "https://nix-community.cachix.org?priority=2"
-      # "https://cuda-maintainers.cachix.org?priority=1"
-    ];
-    extra-trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      #"cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
-    ];
-  };
-
   inputs = {
     # nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     nixpkgs.url = "github:nixos/nixpkgs";
